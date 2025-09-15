831e24b2e445381f01065b91a627400b1029aa3b
9024454 <9024454@student.zadkine.nl>
2025-09-15 14:49:25 +0200

    level 2

--- 1-js-methods/level-2-combinaties/2.2-sort-oefening/app.js
+++ 1-js-methods/level-2-combinaties/2.2-sort-oefening/app.js
@@ -1,20 +1,21 @@
 // Opdracht 2.2: Sort Oefening
 console.log('🚀 Opdracht 2.2: Sort Oefening');
-
+ 
 const scores = [85, 92, 76, 88, 95, 73, 89];
 const steden = ['Amsterdam', 'Utrecht', 'Rotterdam', 'Den Haag', 'Eindhoven'];
-
+ 
 console.log('Originele scores:', scores);
 console.log('Originele steden:', steden);
-
+ 
 // TODO: Sorteer scores van laag naar hoog
-const gesorteerdeScores = /* jouw code hier */
+const gesorteerdeScores = scores.slice().sort((a, b) => a - b);
 console.log('Scores laag naar hoog:', gesorteerdeScores);
-
+ 
 // TODO: Sorteer scores van hoog naar laag  
-const scoresHoogNaarLaag = /* jouw code hier */
+const scoresHoogNaarLaag = scores.slice().sort((a, b) => b - a);
 console.log('Scores hoog naar laag:', scoresHoogNaarLaag);
-
+ 
 // TODO: Sorteer steden alfabetisch
-const gesorteerdeSteden = /* jouw code hier */
+const gesorteerdeSteden = steden.slice().sort();
 console.log('Steden alfabetisch:', gesorteerdeSteden);
+ 
--- 1-js-methods/level-2-combinaties/2.4-complexe-filtering/app.js
+++ 1-js-methods/level-2-combinaties/2.4-complexe-filtering/app.js
@@ -1,21 +1,21 @@
 // Opdracht 2.4: Complexe Filtering
-console.log('🚀 Opdracht 2.4: Complexe Filtering');
+ console.log('🚀 Opdracht 2.4: Complexe Filtering');
 
-const getallen = [5, 12, 8, 23, 15, 3, 18, 25, 7, 30];
+ const getallen = [5, 12, 8, 23, 15, 3, 18, 25, 7, 30];
 const woorden = ['appel', 'banaan', 'aardbei', 'peer', 'ananas', 'bosbes'];
 
-console.log('Alle getallen:', getallen);
-console.log('Alle woorden:', woorden);
+ console.log('Alle getallen:', getallen);
+ console.log('Alle woorden:', woorden);
 
-// TODO: Filter getallen die groter zijn dan 10 EN kleiner dan 20
-const middelsteGetallen = /* jouw code hier */
+// // TODO: Filter getallen die groter zijn dan 10 EN kleiner dan 20
+const middelsteGetallen = getallen.filter(getal => getal > 10 && getal < 20);
 console.log('Getallen tussen 10 en 20:', middelsteGetallen);
 
+
 // TODO: Filter woorden die beginnen met 'a' EN langer zijn dan 5 letters
-const langeAwoorden = /* jouw code hier */
+const langeAwoorden = woorden.filter(woord => woord.startsWith('a') && woord.length > 5);
 console.log('Lange woorden met a:', langeAwoorden);
-
+ 
 // TODO: Filter even getallen die groter zijn dan 15
-const groteEvenGetallen = /* jouw code hier */
+const groteEvenGetallen = getallen.filter(num => num % 2 === 0 && num > 15);
 console.log('Grote even getallen:', groteEvenGetallen);
-
