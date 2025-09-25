1d73bd187365173cc1c4441dfd3a7371ac02b467
9024454 <9024454@student.zadkine.nl>
2025-09-25 17:08:46 +0200

    birthday

--- 3-json/opdracht-01-birthday-reminder/app.js
+++ 3-json/opdracht-01-birthday-reminder/app.js
@@ -1,1 +1,34 @@
-//maak een function met kaas
+let output = document.querySelector('.birthdays');
+let out = '';
+
+fetch('birthday.json')
+    .then(response => response.json())
+    .then(data => {
+        const birthdaysSection = document.querySelector('.birthdays');
+        const title = document.getElementById('birthday-title');
+        title.textContent = `${data.length} Birthdays Today`;
+
+        function renderBirthdays(list) {
+            birthdaysSection.innerHTML = '';
+            list.forEach(person => {
+                const div = document.createElement('div');
+                div.className = 'birthday';
+                div.innerHTML = `
+          <img src="${person.picture}" alt="${person.name}">
+          <div class="birthday-info">
+            <div class="name">${person.name}</div>
+            <div class="age">${person.age} years</div>
+          </div>
+        `;
+                birthdaysSection.appendChild(div);
+            });
+        }
+
+        renderBirthdays(data);
+
+        document.getElementById('clear-btn').addEventListener('click', () => {
+            birthdaysSection.innerHTML = '';
+            title.textContent = '0 Birthdays Today';
+        });
+    })
+    .catch(error => console.log('error', error));
