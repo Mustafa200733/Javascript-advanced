78ab345b63f176bdacb5b2c830a3235920c0eec7
9024454 <9024454@student.zadkine.nl>
2025-10-01 11:36:11 +0200

    fortnite cosmetics

--- 4-api/opdracht-01-fortnite-cosmetics/app.js
+++ 4-api/opdracht-01-fortnite-cosmetics/app.js
@@ -1,30 +1,19 @@
+let output = document.querySelector('#FortniteCosmetics')
+ 
+ 
 fetch('https://fortnite-api.com/v2/cosmetics/new')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('cosmetics');
-    container.classList.add('cosmetics-container');
-    if (data.data && data.data.items && Array.isArray(data.data.items.br)) {
-      if (data.data.items.br.length === 0) {
-        container.innerHTML = "<p>Geen nieuwe cosmetics gevonden.</p>";
-      } else {
-        for (let cosmetic of data.data.items.br) {
-          const div = document.createElement('div');
-          div.classList.add('cosmetic-card'); 
-
-          div.innerHTML = `
-            <h3>${cosmetic.name}</h3>
-            <p>Type: ${cosmetic.type.displayValue}</p>
-            <img src="${cosmetic.images.icon}" alt="${cosmetic.name}" width="100">
-          `;
-          container.appendChild(div);
-        }
-      }
-    } else {
-      container.innerHTML = "<p>Geen nieuwe cosmetics gevonden.</p>";
+.then((response) => response.json())
+.then((data) => {
+    console.log(data.data.items.br);
+    for (let item of data.data.items.br){
+        output.innerHTML += `
+             
+            <h3>${item.name}</h3>
+            <img class="img" src=${item.images.featured}>
+           
+        `;
     }
-  })
-  .catch(error => {
-    console.log('Fout:', error);
-    const container = document.getElementById('cosmetics');
-    container.innerHTML = '<p>Sorry, de cosmetics konden niet geladen worden.</p>';
-  });
+ 
+});
+ 
+ 
